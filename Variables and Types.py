227b400f0b259b1c91cb2101ca08b14436a89df1
500kguser <102877038+500kguser@users.noreply.github.com>
2022-11-07 15:35:54 +0000

    Update Variables and Types.py

--- Variables and Types.py
+++ Variables and Types.py
@@ -1,1 +1,12 @@
 Python supports two types of numbers - integers(whole numbers) and floating point numbers(decimals)
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+Strings are defined either with a single quote or a double quotes.
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
