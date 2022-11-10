d04b984a3c625c3483120795135f37463f09ba9f
ptd3v <tovey@live.co.uk>
2022-11-10 18:31:36 +0000

    Update Basic Operators.py

--- Basic Operators.py
+++ Basic Operators.py
@@ -12,7 +12,37 @@ cubed = 2 ** 3
 print(squared)
 print(cubed)
 
+#Using Operators with Strings
 helloworld = "hello" + " " + "world"
 print(helloworld)
 
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+#Using Operators with Lists
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
+
+#Exercise
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x]
+y_list = [y]
+big_list = []
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
 
