7233b53be66850298ef90e39070b41399a3f36e1
ptd3v <tovey@live.co.uk>
2022-11-10 18:27:45 +0000

    Create Basic Operators.py

---
+++ Basic Operators.py
@@ -0,0 +1,18 @@
+#Arithmetic Operators
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+#Modulo - The remainder
+remainder = 11 % 3
+print(remainder)
+
+#Power of
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+
