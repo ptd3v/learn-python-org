53fa4f04341908ebdd41b3f709c747478d299aed
ptd3v <tovey@live.co.uk>
2022-11-15 14:29:26 +0000

    Summary

---
+++ Conditions.py
@@ -0,0 +1,12 @@
+x = 2
+print(x == 2) # prints out True
+print(x == 3) # prints out False
+print(x < 3) # prints out True
+
+name = "John"
+age = 23
+if name == "John" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
