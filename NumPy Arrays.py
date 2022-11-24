06decb48019d272826b460526d141fe43936f61d
500kguser <102877038+500kguser@users.noreply.github.com>
2022-11-24 12:21:05 +0000

    Update NumPy Arrays.py

--- NumPy Arrays.py
+++ NumPy Arrays.py
@@ -17,3 +17,23 @@ bmi = np_weight / np_height ** 2
 
 # Print the result
 print(bmi)
+
+# For a boolean response
+bmi > 23
+
+# Print only those observations above 23
+bmi[bmi > 23]
+
+weight_kg = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+import numpy as np
+
+# Create a numpy array np_weight_kg from weight_kg
+np_weight_kg = np.array(weight_kg)
+
+# Create np_weight_lbs from np_weight_kg
+np_weight_lbs = np_weight_kg / 2.2
+
+# Print out np_weight_lbs
+print(np_weight_lbs)
+print(np_weight_kg)
