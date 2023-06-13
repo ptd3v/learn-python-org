3ad07b4526b870371491c870fff9816d4f19cafb
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-13 20:59:57 +0100

    Python NumPy Arrays

--- NumPy Arrays.py
+++ NumPy Arrays.py
@@ -1,7 +1,7 @@
 #This is a summary of content and completed test exercise from learnpython.org
 
 #Numpy arrays are great alternatives to Python Lists.
-#Create 2 new lists height and weight
+#Create 2 lists, height and weight.
 height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
 weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
 
