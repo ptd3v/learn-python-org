be4591489051000ae862bca6514950ced4969d5d
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-13 21:02:57 +0100

    Python Regular Expressions

--- Regular Expressions.py
+++ Regular Expressions.py
@@ -1,6 +1,6 @@
 #This is a summary of content and completed test exercise from learnpython.org
 
-#Regular Expressions Example
+#Regular Expressions
 import re
 pattern = re.compile(r"\[(on|off)\]") # Slight optimization
 print(re.search(pattern, "Mono: Playback 65 [75%] [-16.50dB] [on]"))
